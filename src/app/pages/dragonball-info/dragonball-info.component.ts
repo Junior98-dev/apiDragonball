9948b375c485c22f8e87be72e10145bd4262f106
Junior98-dev <kounkouddorianjunior@gmail.com>
2025-05-11 23:18:51 +0200

    Ajout du composant DragonballInfo avec son template et son style associés

---
+++ src/app/pages/dragonball-info/dragonball-info.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dragonball-info',
+  imports: [],
+  templateUrl: './dragonball-info.component.html',
+  styleUrl: './dragonball-info.component.scss'
+})
+export class DragonballInfoComponent {
+
+}
