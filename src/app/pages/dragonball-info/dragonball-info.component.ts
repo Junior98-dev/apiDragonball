b755a3d3485cf7d68f05ace8d0c45c3edb207f0b
Junior98-dev <kounkouddorianjunior@gmail.com>
2025-05-11 23:56:18 +0200

    Modification de l'exportation du composant DragonballInfo en tant que classe par défaut

--- src/app/pages/dragonball-info/dragonball-info.component.ts
+++ src/app/pages/dragonball-info/dragonball-info.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   templateUrl: './dragonball-info.component.html',
   styleUrl: './dragonball-info.component.scss'
 })
-export class DragonballInfoComponent {
+export default class DragonballInfoComponent {
 
 }
