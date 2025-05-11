6f04f552a254eef74bde3f1cb03172c1df50eb68
Junior98-dev <kounkouddorianjunior@gmail.com>
2025-05-11 23:55:48 +0200

    Ajout des routes pour les composants Dragonball et DragonballInfo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        title: 'DragonBall Z',
+        loadComponent: () => import('./pages/dragonball/dragonball.component'),
+    },
+    {
+        path: 'dragonball/:id',
+        loadComponent: () => import('./pages/dragonball-info/dragonball-info.component'),
+    }
+];
