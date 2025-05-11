c5755bd79594fd997ae042cae752e9062412aabf
Junior98-dev <kounkouddorianjunior@gmail.com>
2025-05-11 23:56:00 +0200

    Ajout des composants Dragonball et DragonballInfo dans le module principal et mise à jour du template pour inclure le composant Toolbar.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import DragonballComponent from './pages/dragonball/dragonball.component';
+import DragonballInfoComponent from './pages/dragonball-info/dragonball-info.component';
+import { ToolbarComponent } from './shared/toolbar/toolbar.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    ToolbarComponent,
+    FooterComponent,
+  ],
   template: `
-    <h1>Welcome to {{title}}!</h1>
-
+    <app-toolbar />
     <router-outlet />
+    <app-footer />
   `,
   styles: [],
 })
