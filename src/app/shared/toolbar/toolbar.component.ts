ef3ca637e515dde1c0240df349b12deb4fcb7f2b
Junior98-dev <kounkouddorianjunior@gmail.com>
2025-05-12 22:53:03 +0200

    Réorganisation de la structure du composant Toolbar : mise à jour du titre et ajout de styles pour le conteneur et la barre d'outils.

--- src/app/shared/toolbar/toolbar.component.ts
+++ src/app/shared/toolbar/toolbar.component.ts
@@ -5,11 +5,35 @@ import { Component } from '@angular/core';
   imports: [],
   template: `
    <div class="container-toolbar">
-     <h3>Dragon<span>ball Z</span></h3>
+    <div class="toolbar">
+      <h2>Dragon<span>ball Z</span></h2>
+    </div>  
    </div> 
   `,
-  styles: ``
+  styles: `
+    .container-toolbar {
+      position: sticky;
+      top: 0;
+      z-index: 100;
+      background-color: white;
+      border-bottom: 1px solid #ccc;
+
+    .toolbar {
+      width: 80%;
+      height: 60px;
+      margin: 0 auto;
+      padding: 0.9rem 0;
+
+     span{
+      color: #ffcc00;
+      font-weight: bold;
+     }
+    }
+  }
+
+
+  `
 })
 export class ToolbarComponent {
-
+  
 }
