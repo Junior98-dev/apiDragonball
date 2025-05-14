fb71b6fc759ef27c2f40a99185edb747567500df
Junior98-dev <kounkouddorianjunior@gmail.com>
2025-05-14 23:36:38 +0200

    Ajout de la logique de récupération des personnages dans le composant Dragonball avec intégration de l'interface DragonBallCharacter.

--- src/app/pages/dragonball/dragonball.component.ts
+++ src/app/pages/dragonball/dragonball.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
+import { ApidragonballzService } from '../../core/services/apidragonballz.service';
+import { JsonPipe } from '@angular/common';
+import { DragonBallCharacter } from '../../core/models/Dragonballz.model';
 
 @Component({
   selector: 'app-dragonball',
-  imports: [],
+  imports: [JsonPipe],
   templateUrl: './dragonball.component.html',
   styleUrl: './dragonball.component.scss'
 })
-export default class DragonballComponent {
+export default class DragonballComponent implements OnInit {
+  dragonball?: DragonBallCharacter[];
+  
+  constructor(private api: ApidragonballzService) {}
 
+  ngOnInit() {
+    this.api.getAllCharacters().subscribe((data) => {
+      this.dragonball = data;
+    });
+  }
 }
