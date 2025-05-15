64249d8f35dba46d2a4865058b9093fe8363d0e5
Junior98-dev <kounkouddorianjunior@gmail.com>
2025-05-15 23:37:44 +0200

    Refactor de l'interface DragonBallCharacter : séparation des sous-interfaces pour une meilleure structuration des données

--- src/app/core/models/Dragonballz.model.ts
+++ src/app/core/models/Dragonballz.model.ts
@@ -1,12 +1,33 @@
+export interface Character {
+  id: number;
+  name: string;
+  ki: string;
+  maxKi: string;
+  race: string;
+}
+
+export interface Meta {
+  totalItems: number;
+  itemCount: number;
+  itemsPerPage: number;
+  totalPages: number;
+  currentPage: number;
+}
+
+export interface Links {
+  first: string;
+  previous: string;
+  next: string;
+  last: string;
+}
+
 export interface DragonBallCharacter {
-    id: number;
-    name: string;
-    ki: string;
-    maxKi: string;
-    race: string;
-    gender: string;
-    description: string;
-    image: string;
-    affiliation: string;
-    deletedAt: string | null; // Peut être une chaîne de caractères (date) ou null
-  }
+  items: Character[];
+  meta: Meta;
+  links: Links;
+}
+
+
+
+
+
