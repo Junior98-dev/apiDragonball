029bcd65c0f86644bc9c88f20eb747e6d81c0435
Junior98-dev <kounkouddorianjunior@gmail.com>
2025-05-21 14:43:52 +0200

    Mise à jour de la méthode getAllCharacters pour limiter le nombre de personnages retournés à 12.

--- src/app/core/services/apidragonballz.service.ts
+++ src/app/core/services/apidragonballz.service.ts
@@ -10,7 +10,7 @@ export class ApidragonballzService {
  http = inject(HttpClient);
 
  getAllCharacters() {
-  return this.http.get<DragonBallCharacter>(`${this.Url}/characters`);
+  return this.http.get<DragonBallCharacter>(`${this.Url}/characters?limit=12`);
  }
  getCharacterById(id:number){
   return this.http.get<DragonBallCharacter>(`${this.Url}/characters/${id}`);
